f62e837bc42546d2d2454e343cffd60d8ce4c240
Brennan Ormes <brennanormes@gmail.com>
2022-04-06 22:20:30 +0100

    Added service method for filtering invoices, Added endpoint for filtering, Updated dev diary, Added new status to InvoiceStatus enum

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/InvoiceService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/InvoiceService.kt
@@ -7,6 +7,7 @@ package io.pleo.antaeus.core.services
 import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
 import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
 
 class InvoiceService(private val dal: AntaeusDal) {
     fun fetchAll(): List<Invoice> {
@@ -15,5 +16,15 @@ class InvoiceService(private val dal: AntaeusDal) {
 
     fun fetch(id: Int): Invoice {
         return dal.fetchInvoice(id) ?: throw InvoiceNotFoundException(id)
+    }
+
+    /**
+     * @param status the status to fetch as [String]
+     * @throws IllegalArgumentException
+     */
+    fun fetchAllByStatus(status: String): List<Invoice> {
+        val statusValue = InvoiceStatus.valueOf(status)
+        // Return invoices by status
+        return fetchAll().filter { it.status == statusValue }
     }
 }
