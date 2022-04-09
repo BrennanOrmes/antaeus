6947e522a0b4db82f910a29cc3d39a865177aae0
Brennan Ormes <brennanormes@gmail.com>
2022-04-09 20:00:56 +0100

    Added method for charging invoices

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,50 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
+    private val paymentProvider: PaymentProvider,
+    private val invoiceService: InvoiceService
 ) {
-// TODO - Add code e.g. here
+
+    /**
+     * Charged invoice using [PaymentProvider]
+     * @param invoice as [Invoice]
+     * @return returns updated invoice as [Invoice]
+     * @throws IllegalArgumentException
+     * @throws CurrencyMismatchException
+     * @throws NetworkException
+     * @throws CustomerNotFoundException
+     * @throws InvoiceNotFoundException
+     */
+    fun chargeInvoice(invoice: Invoice): Invoice {
+        // throw not found exception if invoice does not exist.
+        invoiceService.fetch(invoice.id)
+
+        return try {
+            if(paymentProvider.charge(invoice)) {
+                invoiceService.updateInvoice(invoice.id, InvoiceStatus.PAID.toString())
+            } else {
+                invoiceService.updateInvoice(invoice.id, InvoiceStatus.FAILED.toString())
+            }
+        } catch (exception: Exception) {
+            when(exception) {
+                is CurrencyMismatchException, is CustomerNotFoundException -> {
+                    invoiceService.updateInvoice(invoice.id, InvoiceStatus.FAILED.toString())
+                } is NetworkException -> {
+                    // if paymentProvider throws a network error, we will keep the invoice as pending
+                    return invoice
+                } else -> {
+                    // catch any unhandled exceptions here
+                    throw exception
+                }
+            }
+        }
+    }
 }
